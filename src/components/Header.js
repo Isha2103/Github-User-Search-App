0dde9c169549f8a8bd7378713f8f21a107cfbb81
Isha2103 <ishagupta2103@gmail.com>
2022-06-29 17:52:42 +0530

    Added theme toggle functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,34 @@
 import React from "react";
 import MoonIcon from "../images/icon-moon.svg";
+import SunIcon from "../images/icon-sun.svg";
 
 function Header(props) {
   return (
     <header>
       <h1 className="header--title">devfinder</h1>
-     <img className="icon-dark"src={MoonIcon} alt="" onClick={themeToggle}/>
+      <div className="icon-dark" onClick={themeToggle}>
+        <span id="change-theme-name">DARK</span>
+        <div class="img-theme">
+        </div>
+        <img class="toggle-icon-moon" src={MoonIcon} alt=""/>
+        <img class="toggle-icon-sun" src={SunIcon} alt=""/>
+     </div>
     </header>
   );
 }
 
 function themeToggle(){
   const btn = document.querySelector('.icon-dark'); 
+  var x = document.getElementById("change-theme-name");
   btn.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');  
+    document.body.classList.toggle('dark-theme');
+    if (x.innerHTML === "DARK") {
+      x.innerHTML = "LIGHT";
+    } else {
+      x.innerHTML = "DARK";
+    }
   })
+
 }
+
 export default Header;
