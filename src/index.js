95ea68dafd409c72914d5bba49ff2d8497b93761
Isha2103 <ishagupta2103@gmail.com>
2022-07-01 17:16:03 +0530

    added browserrouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { UserDataContextProvider } from "./contexts/userDataContext";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <UserDataContextProvider>
-      <App />
+      <App />    
     </UserDataContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
